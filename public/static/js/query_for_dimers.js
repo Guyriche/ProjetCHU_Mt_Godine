256f2a0fbb71a75df6d114bdfd056326e20b2eb1
Guyriche <guyssel.nindjeu@student.unamur.be>
2022-03-09 11:05:30 +0100

    add Query Js for the dimers.html

---
+++ public/static/js/query_for_dimers.js
@@ -0,0 +1,40 @@
+// get the form from DOM (Document object model)
+
+var form = document.getElementById("dimers");
+var nom_v = document.getElementById("name_output_Dimers");
+
+var TSVFILE = document.getElementById("tsv_file");
+
+form.onsubmit = function (event){
+    var xhr = new XMLHttpRequest();
+    var data = new FormData(form);
+
+    // Add extra data to form before submission
+
+    data.append("Program_Output", nom_v.value);
+    console.log(nom_v.value)
+    var files_list_tsv = [];
+    for (let i = 0; i < TSVFILE.files.length; i++){
+        data.append(TSVFILE.files[i].name, TSVFILE.files[i]);
+        files_list_tsv.push(TSVFILE.files[i].name);
+    }
+    //data.append("Files", files_list_tsv)
+    console.log(nom_v.value);
+    console.log((FASTAFILE.files));
+
+        // open Request
+    xhr.open('POST', 'http://127.0.0.1:5000/dimer');
+
+    //Send the form data
+    xhr.send(data);
+
+    xhr.onreadystatechange = function () {
+        if(xhr.readyState == XMLHttpRequest.DONE){
+            form.reset(); // reset form after AJAX Success.
+        }
+    }
+
+    // Dont Submit the Form
+    return false;
+
+}
